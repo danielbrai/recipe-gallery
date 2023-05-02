8e2e8fc00db847cc0607b5df12e890dd553aa873
DANIEL BRAI GONZALES MARCOS <daniel.marcos@bradesco.com.br>
2023-05-02 08:34:47 -0300

    COMMIT#3
    - Estruturação dos componentes de Detalhes da Receita e Listagem de
      Receita
    - Criação de uma classe de modelo de domínio para representar a Receita

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+
+  public recipes: Recipe[] = [];
+
+  constructor() {
+    this.recipes.push(
+      new Recipe('Ratatouille', 'A French Provençal dish of stewed vegetables.', 'https://img.buzzfeed.com/video-api-prod/assets/eb44570519264864814264f7f0a5e47a/BFV13909_BakedRatatouille-ThumbTextless1080.jpg')
+    )  
+    
+    this.recipes.push(
+      new Recipe('Ratatouille', 'A French Provençal dish of stewed vegetables.', 'https://img.buzzfeed.com/video-api-prod/assets/eb44570519264864814264f7f0a5e47a/BFV13909_BakedRatatouille-ThumbTextless1080.jpg')
+    )
+  }
 
 }
---
+++ src/app/recipes/recipe.model.ts
@@ -0,0 +1,25 @@
+export class Recipe {
+
+    private _name: string;
+    private _description: string;
+    private _imagePath: string;
+
+    constructor(name: string, description: string, imagePath: string) {
+        this._name = name;
+        this._description = description;
+        this._imagePath = imagePath;
+    }
+
+    public get name(): string {
+        return this._name;
+    }
+
+    public get description(): string {
+        return this._description;
+    }
+
+    public get imagePath(): string {
+        return this._imagePath;
+    }
+
+}
