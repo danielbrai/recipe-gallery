29ab8e8b250686c574ab37e4f9d476032cc5d903
DANIEL BRAI GONZALES MARCOS <daniel.marcos@bradesco.com.br>
2023-05-05 06:19:30 -0300

    COMMIT#5
    - Adição do comportamento de nevageção com o uso da diretiva estrutural
      NgIf combinada com Component Data Binding uando EventEmitters e
    Outputs; com essa abordagem, adicinou-se comportamento no click dos
    links da navbar para navegar entre a feature de listagem de receitas e
    de lista de compras.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'recipe-gallery';
+
+  componentToShow: string = null;
+
+  onLinkClick(event) {
+    this.componentToShow = event;
+  }
+
+  shouldShowRecipeComponent(): boolean {
+    return this.componentToShow === 'recipe'
+  }
+
+  shouldShowShoppingListComponent(): boolean {
+    return this.componentToShow === 'shoppingList'
+  }
 }
